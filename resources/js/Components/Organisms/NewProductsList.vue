b62b7f4e66aecf88438647a60420e6fa8cd23810
mutaman97 <mutamanelhadi97@gmail.com>
2024-07-06 16:44:53 +0400

    Fix: Grid colls

--- resources/js/Components/Organisms/NewProductsList.vue
+++ resources/js/Components/Organisms/NewProductsList.vue
@@ -9,7 +9,7 @@ defineProps({
 </script>
 
 <template>
-    <div class="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
+    <div class="grid grid-cols-2 gap-x-3 gap-y-10 sm:grid-cols-3 sm:gap-x-4 md:grid-cols-3 md:gap-x-6 lg:grid-cols-3 lg:gap-x-6 xl:grid-cols-4 xl:gap-x-8">
         <div v-for="(item, index) in items"
              class="group">
             <new-product-card :key="index" :data="item"/>
